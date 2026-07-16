c5837e4cd81c5a55e2fe95998deb0271af660ccf
Vanshika <158816854+Vanshika-OFFICIAL@users.noreply.github.com>
2026-07-16 20:57:42 +0530

    Time: 2 ms (54.32%), Space: 59 MB (84.31%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,14 @@
+var moveZeroes=function(nums){
+    let slow=0;
+    
+    for(let fast=0;fast<nums.length;fast++){
+        
+        if(nums[fast]!==0){
+            let temp = nums[slow];
+        nums[slow] = nums[fast];
+        nums[fast] = temp;
+        slow++;
+        }
+        
+    } return;
+}
